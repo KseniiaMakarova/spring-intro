9c15e7757aba7c2ef1e97c59ce3e2d1a7ce088a8
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-06-03 11:58:21 +0300

    refactored AppConfig class - made DB properties be injected from db.properties file

--- src/main/java/com/intro/spring/service/impl/UserServiceImpl.java
+++ src/main/java/com/intro/spring/service/impl/UserServiceImpl.java
@@ -4,14 +4,12 @@ import com.intro.spring.dao.UserDao;
 import com.intro.spring.model.User;
 import com.intro.spring.service.UserService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private UserDao userDao;
+    private final UserDao userDao;
 
-    @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
